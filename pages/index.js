a4bb87000b330ab0c7462b2a6849f60ed2cfbbf8
Will Shalders <williswebdesign1@gmail.com>
2021-08-08 22:23:34 +1000

    change quotes

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Head from "next/head";
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import SmallCard from "../components/SmallCard";
+import React from 'react';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
 
 export default function Home({ exploreData }) {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Hiatus</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <Header />
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
-        <section className="pt-6">
-          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {exploreData?.map(({ img, location, distance }) => (
               <SmallCard img={img} location={location} distance={distance} />
             ))}
@@ -30,7 +30,7 @@ export default function Home({ exploreData }) {
 }
 
 export async function getStaticProps() {
-  const result = await fetch("https://links.papareact.com/pyp");
+  const result = await fetch('https://links.papareact.com/pyp');
   const exploreData = await result.json();
 
   return {
